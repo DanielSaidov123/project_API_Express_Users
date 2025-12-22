746a22aac678f0e61a03a7cdc09d0350d1dce64a
DanielSaidov123 <ddny3622@gmail.com>
2025-12-22 16:57:59 +0200

    add project

---
+++ server.js
@@ -0,0 +1,119 @@
+import { readFromFile, writeToFile, getNextId } from "./utils.js";
+import express from "express";
+import fs from "fs/promises";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  try {
+    const usersArr = await readFromFile("users.json");
+    res.json(usersArr);
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+app.get("/user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const usersArr = await readFromFile("users.json");
+    const user = usersArr.filter((u) => u.id == id);
+    if (user.length <= 0) {
+      res.status(404).json({ mes: "id is not found" });
+      return;
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(404).json({ err });
+  }
+});
+
+app.get("/users/search", async (req, res) => {
+  try {
+    const { city } = req.query;
+    const users = await readFromFile("users.json");
+    console.log(typeof users);
+    const user = users.filter((u) => u.city === city);
+    if (user.length <= 0) {
+      res.status(404).json({ mes: "id is not found" });
+      return;
+    }
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+app.post("/users", async (req, res) => {
+  try {
+    const targetArr = await readFromFile("users.json");
+    const newUser = {
+      id: getNextId(targetArr),
+      name: req.body?.name || "",
+      age: req.body?.age || 0,
+      city: req.body?.city || "",
+    };
+    targetArr.push(newUser);
+    await writeToFile("users.json", targetArr);
+    res.status(201).json({ msg: "success", data: req.body });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ msg: "error" + err.message, data: null });
+  }
+});
+
+app.put("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { body } = req;
+    const intId = parseInt(id);
+    if (isNaN(intId)) throw new Error("Invalid id, please use an integer.");
+    const ListUsers = await readFromFile("users.json");
+    const user = ListUsers.find((t) => t.id === intId);
+    console.log(user);
+    console.log(body);
+    if (!user) {
+      res.status(404).json({ success: false, data: {} });
+    } else {
+      user.name = body.name || user.name;
+      user.age = body.age || user.age;
+      user.city = body.city || user.city;
+
+      await writeToFile("users.json", ListUsers);
+      res.status(200).json({ success: true, data: user });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, data: error.message });
+  }
+});
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const intId = parseInt(id);
+
+    if (isNaN(intId)) throw new Error("Invalid id, please use an integer.");
+    const users = await readFromFile("users.json");
+    const user = users.find((u) => u.id === intId);
+
+    if (!user) {
+      res.status(404).json({ success: false, data: {} });
+    } else {
+      const indexToDelete = users.findIndex((t) => t.id === intId);
+
+      users.splice(indexToDelete, 1);
+      await writeToFile("users.json", users);
+
+      res.status(200).json({ success: true, data: {} });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, data: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ utils.js
@@ -0,0 +1,38 @@
+import fs from 'fs/promises'
+
+
+
+export async function readFromFile(fileName) {
+  try {
+    const data = await fs.readFile(fileName, "utf8");
+    const arr = JSON.parse(data);
+    console.log(arr)
+    return arr
+  } catch (err) {
+    console.error("Error reading from file:", err);
+    return null;
+  }
+}
+
+
+export async function writeToFile(fileName, data) {
+  try {
+    await fs.writeFile(fileName, JSON.stringify(data, null, 2), "utf8");
+    console.log("File written successfully");
+  } catch (err) {
+    console.error("Error writeing from file:", err);
+  }
+}
+
+export const getNextId = (todos) => {
+  if (!todos || todos.length === 0) {
+    return 1;
+  }
+  let maxValue = 0;
+  todos.forEach((todo) => {
+    if (todo.id > maxValue) {
+      maxValue = todo.id;
+    }
+  });
+  return maxValue + 1;
+};
